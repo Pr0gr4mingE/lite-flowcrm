40c8b2163ff90f1cd08b0862b10d23a144edb94b
Progr4ming3 <victoremiliano.purificacao@gmail.com>
2026-08-19 22:41:50 -0300

    refactor(db): atualizar configuracao de conexao do drizzle
    - Refatoracao do arquivo de banco de dados.
    - Implementacao do padrao Singleton para evitar estouro de pool no ambiente de desenvolvimento.

--- src/infrastructure/database/db.ts
+++ src/infrastructure/database/db.ts
@@ -1,11 +1,20 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as usuarioSchema from "./schemas/usuario.schema";
-// No futuro, você pode importar os outros schemas aqui se for usar o db.query.findFirst...
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+// 1. Garantimos que a string de conexão do .env não seja undefined
+const connectionString = process.env.DATABASE_URL!;
 
-// Exportamos a instância 'db' tipada com os schemas para habilitar queries avançadas
+// 2. Tipagem global para evitar conflitos no TypeScript
+const globalForPg = globalThis as unknown as { pool: Pool };
+
+// 3. Padrão Singleton: usa o pool existente se houver, ou cria um novo
+const pool = globalForPg.pool || new Pool({ connectionString });
+
+// 4. Salva o pool globalmente apenas em ambiente de desenvolvimento
+if (process.env.NODE_ENV !== "production") {
+  globalForPg.pool = pool;
+}
+
+// Exportamos a instância 'db' tipada
 export const db = drizzle(pool, { schema: { ...usuarioSchema } });
